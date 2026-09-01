555a02a74db96718ee4af40e0a8d00c6d6639866
Midhun chacko <thomasvattapparayail@gmail.com>
2026-09-01 16:18:35 +0530

    Email settings updated in settings.py

--- portifolio/portifolio/settings.py
+++ portifolio/portifolio/settings.py
@@ -34,6 +34,18 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['portfolio-tblq.onrender.com']
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 # Application definition
 
 INSTALLED_APPS = [
